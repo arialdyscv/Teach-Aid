08be229aa9f0fcca1813fc85ffc0d930e1930b7d
Cristians <pestanaxd99@gmail.com>
2022-08-10 15:56:57 -0400

    LoginJWTBackend Integrado

---
+++ backend/src/main/java/com/RestApi/TeachAid/controller/HelloWorldController.java
@@ -0,0 +1,17 @@
+package com.RestApi.TeachAid.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+	@RequestMapping({ "/hello" })
+	public String firstPage() {
+		return "Hello World";
+	}
+
+	@RequestMapping({ "/nuevo" })
+	public String secondPage(){ return "Second page"; }
+
+}
